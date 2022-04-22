af88144ac6531a5acdf0f9747415057d14c84b8f
MuhammedRefaat <mrefaat.7889@gmail.com>
2022-04-22 12:33:44 +0400

    Android v2 embidding support

---
+++ android/src/main/java/com/jordanalcaraz/audiorecorder/audiorecorder/MainActivity.java
@@ -0,0 +1,8 @@
+package com.jordanalcaraz.audiorecorder.audiorecorder;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // You can keep this empty class or remove it. Plugins on the new embedding
+    // now automatically registers plugins.
+}
